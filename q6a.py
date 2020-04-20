ea665446bfeb246c464a8612b3ae40cadbb8398d
josephrecord <joseph.record@gmail.com>
2020-04-19 22:05:02 -0400

    solution for 6a

--- q6a.py
+++ q6a.py
@@ -1,15 +1,56 @@
-# with open('q6a.txt') as f:
-#     raw = f.readlines()
+def count_orbits(d, p):
+    assert type(p) == str, "plant needs to be a string"
+    assert type(d) == dict, "1st arg needs to be dictionary"
+    count_orbits.counter += 1
+    prev = d.get(p)
+    if prev in d:
+        return count_orbits(d, prev)
+    else:
+        return count_orbits.counter
 
-planets = set()
 
-with open('q6a-testinput.txt') as f:
+def prev_planet(d, p):
+    prev = d.get(p)
+    return prev
+
+
+orbitee_list = []
+orbiter_list = []
+
+solar_sys = {}
+
+with open('q6a.txt') as f:
     test_input = f.readlines()
     for line in test_input:
-        striped_line = line.rstrip()
+        striped_line = line.rstrip() # remove newline char
         sym_pos = striped_line.find(")")
-        planets.add(striped_line[:sym_pos])
-        planets.add(striped_line[sym_pos+1:])
+        orbitee = striped_line[:sym_pos]
+        orbiter = striped_line[sym_pos+1:]
+        # solar_sys.setdefault(orbiter, [])
+        solar_sys[orbiter] = orbitee
+        orbitee_list.append(orbitee)
+        orbiter_list.append(orbiter)
+        print(orbiter, " Directly orbits ", orbitee)
 
 
-print(planets)
+planets = set(orbitee_list+orbiter_list)
+print("planets len ", len(planets), " :", planets)
+
+print("orbitees ", "len ", len(orbitee_list), orbitee_list, set(orbitee_list))
+print("orbiters ", "len ", len(orbiter_list), orbiter_list, set(orbiter_list))
+
+print("solar system: ", solar_sys)
+
+for planet in planets:
+    count = 0
+    print("planet: ", planet, "- ", solar_sys.get(planet))
+
+
+count_orbits.counter = 0
+
+for planet in planets:
+    print(count_orbits(solar_sys, planet))
+
+ans = count_orbits.counter - 1
+
+print("ans is: ", ans)
