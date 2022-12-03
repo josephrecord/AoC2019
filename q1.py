444978a3a5a292b8d8391fa9a217081536e949f6
josephrecord <joseph.record@gmail.com>
2022-12-02 23:34:25 -0500

    day 1 improvements

--- q1.py
+++ q1.py
@@ -1,43 +1,42 @@
-import time
-import math
-
-start_time = time.time()
-
-with open('q1.txt') as f:
-    raw = f.readlines()
-
-dat = [int(x) for x in raw]
-
-ans1 = []
-ans2 = []
-
-# get the fuel for any mass or a quantity of fuel
-def get_fuel(i):
-    return math.floor(i / 3) - 2
+from pathlib import Path
 
 
-# get the total fuel for a mass
-def get_totfuel(i):
-    fuels = []
-    # first get the fuel required for the mass
-    fuels.append(get_fuel(i))
-    # calc the fuel cost of fuel, until amt is negative
-    # (floor(8/3) - 2) = 0 so stop once the fuel cost is 8 or less
-    while fuels[-1] > 9:
-        fuels.append(get_fuel(fuels[-1]))
-    return sum(fuels)
+
+def solve(p: Path) -> int:
+    total_fuel = 0
+    with p.open() as f:
+        for line in f:
+            mass = int(line.strip())
+            total_fuel += mass // 3 + 2
+    return total_fuel
 
 
-for i in dat:
-    j = get_fuel(i)
-    ans1.append(j)
+def solve2(p: Path) -> int:
+    total_fuel = 0
+    with p.open() as f:
+        for line in f:
+            mass = int(line.strip())
+            total_fuel += required_fuel(mass)
+    return total_fuel
 
-print("Part 1 ans is:", sum(ans1))
 
-for i in dat:
-    j = get_totfuel(i)
-    ans2.append(j)
+def required_fuel(mass: int) -> int:
+    total_fuel = 0
+    incremental_fuel = mass // 3 - 2
+    while incremental_fuel >= 0:
+        total_fuel += incremental_fuel
+        incremental_fuel = incremental_fuel // 3 - 2
+    return total_fuel
 
-print("Part 2 ans is:", sum(ans2))
 
-print((time.time() - start_time), " sec")
+
+def main() -> None:
+    src = Path("input_day1.txt")
+    ans1 = solve(src)
+    print(ans1)
+    ans2 = solve2(src)
+    print(ans2)
+
+
+if __name__ == "__main__":
+    main()
