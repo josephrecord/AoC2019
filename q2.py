96d2cabad1c4e9e6ca7ab7fcb44e7598d68cdc36
josephrecord <joseph.record@gmail.com>
2022-12-03 00:22:26 -0500

    day 2 part 1

--- q2.py
+++ q2.py
@@ -1,41 +1,52 @@
-import sys
 
+prog = [1, 9, 10, 3, 2, 3, 11, 0, 99, 30, 40, 50]
+prog2 = [2,4,4,5,99,0]
 
-def run_prog(intcode):
-    ip = 0
-    opcode = intcode[ip]
-    while True:
+with open("input_day2.txt") as f:
+    input = [int(x) for x in f.readline().split(",")]
+
+# before running the program, replace position 1 with the value 12
+# and replace position 2 with the value 2. What value is left at 
+# position 0 after the program halts?
+input[1] = 12
+input[2] = 2
+
+def run(prog):
+    # start at beginning of program
+    index = 0
+    # get opcode
+    opcode = prog[index]
+    while opcode != 99:
         if opcode == 1:
-            param1, param2, param3 = intcode[ip + 1], intcode[ip + 2], intcode[ip + 3]
-            intcode[param3] = intcode[param1] + intcode[param2]
-            ip = ip + 4
+            # Addition
+            print(f"Addition: {prog[index:index+4]}")
+            operand_1_location = prog[index + 1]
+            operand_2_location = prog[index + 2]
+            output_location = prog[index + 3]
+            opperand1 = prog[operand_1_location]
+            opperand2 = prog[operand_2_location]
+            output_value = opperand1 + opperand2
+            print(output_value)
+            # Overwrite the value at `output_location`
+            prog[output_location] = output_value
+            # Move the program counter
+            index = index + 4
         elif opcode == 2:
-            param1, param2, param3 = intcode[ip + 1], intcode[ip + 2], intcode[ip + 3]
-            intcode[param3] = intcode[param1] * intcode[param2]
-            ip = ip + 4
-        elif opcode == 99:
-            return intcode[0]
-            break
-        else:
-            print("ERROR")
-            sys.exit()
-        opcode = intcode[ip]
+            # Multiplication
+            print(f"Multiplication: {prog[index:index+4]}")
+            operand_1_location = prog[index + 1]
+            operand_2_location = prog[index + 2]
+            output_location = prog[index + 3]
+            opperand1 = prog[operand_1_location]
+            opperand2 = prog[operand_2_location]
+            output_value = opperand1 * opperand2
+            print(output_value)
+            # Overwrite the value at `output_location`
+            prog[output_location] = output_value
+            # Move the program counter
+            index = index + 4
+        opcode = prog[index]
+    return prog
 
-
-initstate = (1, 0, 0, 3, 1, 1, 2, 3, 1, 3, 4, 3, 1, 5, 0, 3, 2, 13, 1, 19, 1, 19, 9, 23, 1, 5, 23, 27, 1, 27, 9, 31, 1, 6, 31, 35, 2,
-35, 9, 39, 1, 39, 6, 43, 2, 9, 43, 47, 1, 47, 6, 51, 2, 51, 9, 55, 1, 5, 55, 59, 2, 59, 6, 63, 1, 9, 63, 67, 1, 67, 10,
-71, 1, 71, 13, 75, 2, 13, 75, 79, 1, 6, 79, 83, 2, 9, 83, 87, 1, 87, 6, 91, 2, 10, 91, 95, 2, 13, 95, 99, 1, 9, 99, 103,
-1, 5, 103, 107, 2, 9, 107, 111, 1, 111, 5, 115, 1, 115, 5, 119, 1, 10, 119, 123, 1, 13, 123, 127, 1, 2, 127, 131, 1,
-131, 13, 0, 99, 2, 14, 0, 0)
-
-# initstate2 = (1,9,10,3,2,3,11,0,99,30,40,50)
-
-for noun in range(100):
-    for verb in range(100):
-        intcode = list(initstate)
-        intcode[1], intcode[2] = noun, verb
-        ans = run_prog(intcode)
-        if ans == 19690720:
-            print(noun, verb)
-            print(100 * noun + verb)
-            sys.exit()
+run(input)
+        
